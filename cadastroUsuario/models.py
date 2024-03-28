0b3b0c6b96bc1309b573bcdb1ef6ac8fe42a3848
apmbraga <braga.ap95@gmail.com>
2024-03-28 19:00:09 -0300

    Criação do models.py onde está a base de dados de notícias, a de cadastro de autores e a de cadastro de categorias. É um prototipo inicial que pode ser modificado depois dependendo da usabilidade. Ainda não foi criado o aplicativo referente a essa parte do projeto, o models.py está como um arquivo solto dentro da pasta geral para ser acrescentado ao seu respectivo lugar depois

--- cadastroUsuario/models.py
+++ cadastroUsuario/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Cadastro(models.Model):
-    Sexo_Choice = (
+    Genero_Choice = (
         ('F', 'Feminino'),
         ('M', 'Masculino'),
         ('O', 'Outros'),
@@ -9,7 +9,7 @@ class Cadastro(models.Model):
 
     nome = models.CharField(max_length=50)
     sobrenome = models.CharField(max_length=50)
-    sexo = models.CharField(max_length=10, choices=Sexo_Choice)
+    Genero = models.CharField(max_length=10, choices=Genero_Choice)
     usuario = models.CharField(max_length=15)
     email = models.EmailField(max_length=75)
     senha = models.CharField(max_length=50)
