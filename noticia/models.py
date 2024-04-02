d985b5142b9c9007b9d9b7d8e0e612cc30baa204
DanieleFG <daniele-fg@hotmail.com>
2024-04-02 18:45:31 -0300

    Novos ajustes na interface

--- noticia/models.py
+++ noticia/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 from django_ckeditor_5.fields import CKEditor5Field
+from django.core.files.storage import default_storage
+import os
 
-
+def upload_to(instance, filename):
+    # Obtém a extensão do arquivo
+    ext = filename.split('.')[-1]
+    # Gera um novo nome para o arquivo
+    new_filename = f'uploads/noticias/{instance.pk}.{ext}'
+    # Retorna o caminho completo para salvar o arquivo
+    return new_filename
 class Autor(models.Model):
     status_Choice = (
         ('A', 'Ativo'),
@@ -40,6 +48,18 @@ class Noticia(models.Model):
     id_categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
     id_autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
     data_publicacao = models.DateTimeField(auto_now_add=True)
+    imagem = models.ImageField(upload_to=upload_to, blank=True, null=True)
+    def save(self, *args, **kwargs):
+        # Verifica se o conteúdo tem uma imagem
+        if '<img src="' in self.conteudo:
+            # Obtém o caminho da imagem no conteúdo
+            start = self.conteudo.find('<img src="') + len('<img src="')
+            end = self.conteudo.find('"', start)
+            img_path = self.conteudo[start:end]
+            # Copia a imagem para o novo local
+            with default_storage.open(img_path, 'rb') as f:
+                self.imagem.save(os.path.basename(img_path), f)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"ID: {self.pk}, titulo: {self.titulo}, Data: {self.data_publicacao},  texto: {self.conteudo}"
